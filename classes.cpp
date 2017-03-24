96f2fe0653597ed682ec1061d6d8b083a2e7e0a7
Gabriel Lecomte <gabriel1906@gmail.com>
2017-03-24 01:31:04 -0300

    Adds print function

--- classes.cpp
+++ classes.cpp
@@ -6,17 +6,28 @@ void Program::codeGen(std::ofstream &out) {
 	out << ".align 2" << std::endl;
 	for (Declaration *d : decs) {
 		if (DecVar *dv = dynamic_cast<DecVar *>(d)) {
-			out << 	dv->identificator << ": .space 4" << std::endl;
+			out << "var" << dv->identificator << ": .space 4" << std::endl;
 		}
 	}
 	out << ".text" << std::endl;
+	out << ".globl main" << std::endl;
 	for (Declaration *d : decs) {
 		if (DecFunc *df = dynamic_cast<DecFunc *>(d)) {
 			if (df->identificator == "main") {
-				out << "main:" << std::endl;
+				df->codeGen(out);
 				out << "\t" << "li $v0, 10" << std::endl;
 				out << "\t" << "syscall" << std::endl;
+				break;
 			}
 		}
 	}
+	out << "print:" << std::endl;
+	out << "\tlw $a0, 4($sp)" << std::endl;
+	out << "\taddiu $sp, $sp, 4" << std::endl;
+	out << "\tli $v0, 1" << std::endl;
+	out << "\tsyscall" << std::endl;
+	out << "\tli $a0, 10" << std::endl;
+	out << "\tli $v0, 11" << std::endl;
+	out << "\tsyscall" << std::endl;
+	out << "\tjr $ra" << std::endl;
 }
