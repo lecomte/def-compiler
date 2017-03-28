0947665feed00a5ef6bdd81432047357c882cc0d
Gabriel Lecomte <gabriel1906@gmail.com>
2017-03-28 16:42:35 -0300

    Addapts testing script to code generation

--- teste.sh
+++ teste.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-for i in {1..9}
+for i in {1..16}
 do
-	./compiler dsamples/0$i.def dsamples/0$i.out
-	echo "0$i.out"
-	diff -w dsamples/0$i.out dsamples/out0$i.def
-done
-for i in {10..17}
-do
-	./compiler dsamples/$i.def dsamples/$i.out
-	echo "$i.out"
-	diff -w dsamples/$i.out dsamples/out$i.def
+	./compiler gsamples/in/prog$i.def gsamples/out/prog$i.S
+	echo "$i.S"
+	cat gsamples/in/prog$i.def
+	spim -file gsamples/out/prog$i.S
 done
